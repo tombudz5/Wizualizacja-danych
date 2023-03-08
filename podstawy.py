ff620d982a7c996d39db290157e448783b3eb1b3
tombudz5 <115640638+tombudz5@users.noreply.github.com>
2023-03-08 11:40:18 +0100

    github edit

--- podstawy.py
+++ podstawy.py
@@ -14,6 +14,7 @@ lista.sort() # sortuje liste
 print(lista)
 lista.pop(6) # usuwaniei elementy po indexie, jezeli nie ma podanego indexu to ostatni element
 print(lista)
+#komentarz z github'a
 
 
 

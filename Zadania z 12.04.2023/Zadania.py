48d9aa94e331d37155f977aaf6eca2834892450d
Tomasz Budzejko <tomek.budzejko@gmail.com>
2023-04-12 12:54:43 +0200

    Complete 3 exercises

---
+++ Zadania z 12.04.2023/Zadania.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+# # ZADANIE 1
+#
+# tablica = np.arange(0, 4*20, 4)
+# print(tablica)
+
+# # ZADANIE 2
+#
+# a = np.arange(5, dtype="float64")
+#
+# b = a.astype("int32")
+#
+# print(a.dtype)
+# print(b.dtype)
+
+# # ZADANIE 3
+#
+# def funkcja(n):
+#     macierz = np.array([2**i for i in range(n**2)])
+#     macierz = macierz.reshape((n, n))
+#     return macierz
+#
+# print(funkcja(5))
+
+

73efd70c05673f99d6c0f4489f72f501f2d58c04
tombudz5 <tomek.budzejko@gmail.com>
2023-05-24 12:14:30 +0200

    New files from 24.05.2023

--- Zadania z 17.05.2023/PracaDomowa/zadanie4.py
+++ Zadania z 17.05.2023/PracaDomowa/zadanie4.py
@@ -1,13 +1,24 @@
 import matplotlib.pyplot as mpl
 import numpy as np
 import pandas as pd
+#
+# df = pd.read_csv('iris.data', delimiter=',')
+# data = {'x': np.array(df.iloc[:, 0].values),
+#         'y': np.array(df.iloc[:, 1].values),
+#         's': np.array([abs(x-y) for x, y in zip(df.iloc[:, 0].values, df.iloc[:, 1].values)])}
+#
+# color_values = np.random.rand(df.shape[0])
+#
+# mpl.scatter('x', 'y', c=color_values, s='s', data=data, cmap='plasma')
+# mpl.show()
 
-df = pd.read_csv('iris.data', delimiter=',')
-data = {'x': np.array(df.iloc[:, 0].values),
-        'y': np.array(df.iloc[:, 1].values),
-        's': np.array([abs(x-y) for x, y in zip(df.iloc[:, 0].values, df.iloc[:, 1].values)])}
-
-color_values = np.random.rand(df.shape[0])
-
-mpl.scatter('x', 'y', c=color_values, s='s', data=data, cmap='plasma')
-mpl.show()
+# # ZADANIE 4
+# iris_data = pd.read_csv('iris.data', decimal='.', sep=',')
+# x = iris_data.iloc[:, 0]
+# y = iris_data.iloc[:, 1]
+#
+# plt.scatter(x, y, c=np.random.randint(0, 149, 149), s=np.abs(x - y))
+# plt.xlabel('sepal length')
+# plt.ylabel('sepal width')
+# plt.title('Wykres punktowy Iris')
+# plt.show()
---
+++ Zadania z 24.05.2023/main.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as mpl
+import pandas as pd
+import seaborn as sns
+from PIL import Image
+
+# WYKRESY Z POPRZEDNIEGO WYKŁADU KTÓRYCH NIE POKAZAŁ
+
+# ts = pd.Series(np.random.randn(1000))
+# ts = ts.cumsum()
+#
+# df = pd.DataFrame(ts, columns=['wartości'])
+# print(df)
+# df['średnia krocząca'] = df.rolling(window=50).mean()
+# df.plot()
+# mpl.legend()
+# mpl.show()
+
+# x = np.random.randn(10000)
+# mpl.hist(x, bins=100, facecolor='y', alpha=0.8, density=True)
+# mpl.xlabel('Wartości')
+# mpl.ylabel('Prawdopodobieństwo')
+# mpl.title('Histogram')
+# mpl.grid()
+# mpl.show()
+
+# df = pd.read_csv('dane.csv', header=0, sep=';', decimal='.')
+# print(df)
+# seria = df.groupby('Imię i nazwisko')['Wartość zamówienia'].sum()
+# wedges, texts, autotext = mpl.pie(x=seria, labels=seria.index, autopct=lambda pct: "{:.2f}%".format(pct), textprops=dict(color='black'), colors=['yellow', 'green'])
+# mpl.title('Suma zamówień dla sprzedawców')
+# mpl.legend(loc='lower right')
+# mpl.ylabel('Procentowy wynik wartości zamówienia')
+# mpl.show()
+
+# SEABORN
+
+# wykres liniowy
+# sns.set(rc={'figure.figsize': (8, 8)})
+# sns.lineplot(x=[1, 2, 3, 4], y=[1, 4, 9, 16],label='linia nr1', color='yellow', marker='o', linestyle='-')
+# sns.lineplot(x=[1, 2, 3, 4], y=[2, 5, 10, 17],label='linia nr2', color='black', marker='^', linestyle='-')
+# mpl.xlabel('oś x')
+# mpl.ylabel('oś y')
+# mpl.title('Wykres liniowy')
+# mpl.show()
+
+
+data = {'Kraj': ['Belgia', 'Indie', 'Brazylia', 'Polska'],'Stolica': ['Bruksela', 'New Delhi', 'Brasilia', 'Warszawa'],'Kontynent': ['Europa', 'Azja', 'Ameryka Południowa', 'Europa'],'Populacja': [11190846, 1303171035, 207847528, 38675467]}
+df = pd.DataFrame(data)
+sns.set()
+plot = sns.barplot(data=df, x='Kontynent', y='Populacja', ci=None, hue='Kontynent', estimator=np.sum, dodge=False, palette=['red', 'green', 'yellow'])
+plot.legend(title='Populacja na kontynentach')
+plot.set(title='Wykres słupkowy')
+mpl.show()
